5b98ad4bf3849675fbc9dd2b271fc4ea1b4f4e83
nauflabhy <n4uf4l.ku@gmail.com>
2025-05-07 11:59:41 +0700

    Merapikan

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -81,7 +81,7 @@ void binarySearch(){
             int mid = (low + high) / 2;
             if (element[mid] == x)
             {
-                cout << "\n[✓] Elemen" << x << " ditemukan pada indeks" << mid << ".\n";
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
                 return;
             }
             if (x < element[mid])
